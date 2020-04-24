db60d7d9ed5c76028af0945136e55a844aef3223
Maspufatullailiyah <maspufa@gmail.com>
2020-04-23 21:22:11 -0700

    Input dengan InputDialog

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -1,7 +1,9 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[] args){
+ Scanner scanner = new Scanner(System.in);
 
   String namaDepan = "Mas";
   String namaTengah = "Pufa";
@@ -12,14 +14,27 @@ public class DataTypes{
   char nilaiAbjad ='A';
   boolean imut = true;
 
+  System.out.print("Input Nama Depan : ");
+  namaDepan = scanner.nextLine();
+  System.out.print("Input Usia : ");
+  usia = scanner.nextInt();
+  System.out.print("Input IPK : ");
+  ipk = scanner.nextDouble();
+  System.out.print("");
+  System.out.print("Input Nilai Abjad : ");
+  nilaiAbjad = scanner.next().charAt(0);
+  System.out.print("imut?: ");
+  imut = scanner.nextBoolean();
+
+  System.out.println("================ OUTPUT ==================");
   System.out.println("Nama depan : " + namaDepan);
   System.out.println("Nama tengah : " + namaTengah);
   System.out.println("Nama belakang : " + namaBelakang);
   System.out.println("Usia : " + usia);
-  System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+  System.out.println("Target Kuliah : " + targetTahunKuliah + "tahun");
   System.out.println("IPK : " + ipk);
-  System.out.println("Nilai PBO : " + nilaiAbjad);
-  System.err.println("Imut : " + imut);
+  System.out.println("Nilai PBO : " + namaAbjad);
+  System.err.println("imut : " + imut);
 
   JOptionPane.showMessageDialog(null,"Hai, " + namaDepan + namaTengah + namaBelakang);
  }
