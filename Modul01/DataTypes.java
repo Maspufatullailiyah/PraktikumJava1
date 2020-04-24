026a163467cfe3cf8003ef8596091e68ab354772
Maspufatullailiyah <maspufa@gmail.com>
2020-04-23 20:06:28 -0700

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+  String namaDepan = "Mas";
+  String namaTengah = "Pufa";
+  String namaBelakang = "Tullailiyah";
+  int usia = 23;
+  int targetTahunKuliah =4;
+  double ipk = 3.89764512;
+  char nilaiAbjad ='A';
+  boolean imut = true;
+ 
+ }
+}
