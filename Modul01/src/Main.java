215319091a6cfec3c7622a5ab1053cb178d8f0f6
Maspufatullailiyah <maspufa@gmail.com>
2020-04-23 21:46:34 -0700

    Project IDEA

---
+++ Modul01/src/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args) {
+
+        String namaDepan = "Mas";
+        String namaTengah = "Pufa";
+        String namaBelakang = "Tullailiyah";
+        int usia = 23;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean imut = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
+
